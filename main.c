ef3b1e57f29df2a629f9a8f3b82721cbb13806c7
Jeon-Ji-Woo <jiwoo3514@sookmyung.ac.kr>
2023-11-05 15:21:52 +0900

    실습2-2

--- main.c
+++ main.c
@@ -8,14 +8,19 @@
 int main(int argc, char *argv[]) 
 {
 	int grade[N_STUDENT];
-	int i;
+	int i, average;
+	int sum;
 	printf("input 5 scores:");
 	
+	sum = 0;
 	for (i=0;i<N_STUDENT;i++)
-		scanf("%d", &grade[i]);    
-	
-	for (i=0;i<N_STUDENT;i++)
-	    printf("grade[%d] = %d\n", i, grade[i]);
+	{
+		scanf("%d", &grade[i]); 
+		sum +=grade[i];
+	}
+		   
+	average = sum / N_STUDENT;
+	printf("score avreage : %i\n", average);
 		
 	system("PAUSE");
 	return 0;
