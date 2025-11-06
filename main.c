f4d1fe3186dc7b81e2c686488957246c133211ca
Jeon-Ji-Woo <jiwoo3514@sookmyung.ac.kr>
2025-11-06 15:30:10 +0900

    실습3

--- main.c
+++ main.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-int main(void) {
-	char code1 = 'A';
-	int code2 = 65;
-	printf("code1 = %c, (%d)\n", code1, code1);
-	printf("code2 = %c, (%d)\n", code2, code2);
-	return 0;
+void main(void) {
+	char src[] = "The worst things to eat before you sleep";
+	char dst[100];
+	strcpy(dst, src);
+	printf("copied string : %s\n", dst);
 }
