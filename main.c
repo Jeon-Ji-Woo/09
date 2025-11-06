dee0bd1fe692307141ca2c8949c066f6d44fac10
Jeon-Ji-Woo <jiwoo3514@sookmyung.ac.kr>
2025-11-06 15:44:29 +0900

    실습5

--- main.c
+++ main.c
@@ -5,6 +5,16 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 void main(void) {
-	char str[30] = "happy C programming";
-	printf("문자열 %s의 길이 : %i", str, strlen(str));
+	char input[100];
+	FILE *fp;
+	int i;
+	
+	fp = fopen("C:\sample.txt", "w");
+	
+	for (i=0;i<3;i++){
+		printf("input a word: ");
+		scanf("%s",input); //문자열이라서 s, 배열이름은 & 안씀. 
+		fprintf(fp, "%s\n", input); 
+	}
+	fclose(fp);
 }
